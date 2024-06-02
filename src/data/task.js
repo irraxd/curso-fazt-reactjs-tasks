a51ea7ca44770f85f2d72ded8fe1a15bea58c55e
irraxd <irraxdgonz7@gmail.com>
2024-06-02 03:07:41 -0400

    Primer commit

---
+++ src/data/task.js
@@ -0,0 +1,17 @@
+export const task = [
+    {
+        id: 0,
+        title: 'Tarea 1',
+        description: 'Mi primera Tarea',
+    },
+    {
+        id: 1,
+        title: 'Tarea 2',
+        description: 'Mi segunda Tarea',
+    },
+    {
+        id: 2,
+        title: 'Tarea 3',
+        description: 'Mi tercera Tarea',
+    },
+];
